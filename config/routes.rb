7c667ca03441890b0b80c606bf05515d7d4e5bde
Ygam Retuta <ygam.j.retuta@gmail.com>
2012-06-01 18:15:18 +0800

    Custom route for businesses

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Jinsei::Application.routes.draw do
 
-  match '/businesses/:name', :to => 'businesses#show', :as => 'business_name'
+  match '/b/:name', :to => 'businesses#show', :as => 'business_name'
 
   root :to => "businesses#index"
   resources :businesses
