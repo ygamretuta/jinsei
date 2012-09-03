a1a46e265cac0c55dc235acb88a29698adda8108
Ygam Retuta <ygam.j.retuta@gmail.com>
2012-09-04 07:39:10 +0800

    Remove begin-rescue block in omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,12 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
-    begin
-      @user = User.find_for_facebook_oauth(request.env['omniauth.auth'], current_user)
-    rescue ActiveRecord::RecordInvalid
-      flash[:notice] = t "app.oauth_error_message"
-      redirect_to login_path
-    end
+    @user = User.find_for_facebook_oauth(request.env['omniauth.auth'], current_user)
 
     if @user.persisted?
       flash[:notice] = t 'devise.omniauth_callbacks.success', :kind => 'Facebook'
